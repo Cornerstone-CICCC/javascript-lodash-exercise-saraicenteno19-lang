97317ed26fdf1c715950192a849e2f366ee12af9
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-06-03 15:06:35 -0400

    Complete lodash exercise 04

--- ex04.js
+++ ex04.js
@@ -4,6 +4,10 @@ const _ = require('lodash');
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// your code here
+const sumOfEvens = _.chain(numbers)
+.filter(num => num %2 === 0)
+.sum()
+.value();
+
 
 console.log(sumOfEvens); // Expected output: 20
