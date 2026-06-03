d3b44fd71b7ff0e664686a6883b9428751c63374
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-06-03 15:20:24 -0400

    Complete lodash exercise 08

--- ex08.js
+++ ex08.js
@@ -4,6 +4,6 @@ const _ = require('lodash');
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// your code here
+const groupedNumbers = _.groupBy(numbers, (num) => (num % 2 === 0 ? 'even' : 'odd'));
 
 console.log(groupedNumbers); // Expected output: { 'odd': [1, 3, 5, 7], 'even': [2, 4, 6, 8] }
